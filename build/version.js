957b61fd3717f03b236766fbbb809751d6817436
afeiship <1290657123@qq.com>
2019-04-25 13:43:06 +0800

    add: sync version

--- build/version.js
+++ build/version.js
@@ -1,8 +1,8 @@
-(function () {
-
+(function() {
   'use strict';
 
   var gulp = require('gulp');
+  var fs = require('fs');
   var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*', 'gulp.*', 'del']
   });
@@ -12,10 +12,10 @@
    * http://www.jianshu.com/p/d616d3bf391f
    */
 
-  gulp.task('bump', function () {
-    gulp.src(['./*.json'])
-      .pipe($.bump())
-      .pipe(gulp.dest('./'));
+  gulp.task('version', function() {
+    var srcPkg = require('../package.json');
+    var targetPkg = require('../.tmp/bulma/package.json');
+    srcPkg.version = targetPkg.version;
+    fs.writeFileSync('./package.json', JSON.stringify(srcPkg, null, 2));
   });
-
-}());
+})();
